5105b9dcc42ebcd9e4f57710242754ccbd588668
xinxiang <xxiang@mail.yu.edu>
2025-04-23 17:48:55 -0400

    Initial Commit

---
+++ Lightin0Tests/Lightin0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lightin0Tests.swift
+//  Lightin0Tests
+//
+//  Created by 向鑫 on 4/23/25.
+//
+
+import Testing
+@testable import Lightin0
+
+struct Lightin0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
